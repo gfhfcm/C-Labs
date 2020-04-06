d7b6fd2a4534c803affb46e2e15c99d21cc9196e
gfhfcm <46087238+gfhfcm@users.noreply.github.com>
2020-04-06 13:25:05 +0300

    Add files via upload

---
+++ lab7/lab7.1/Button.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace lab7._1
+{
+    partial class Running_Button
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button.Location = new System.Drawing.Point(218, 206);
+            this.button.Margin = new System.Windows.Forms.Padding(6);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(206, 81);
+            this.button.TabIndex = 0;
+            this.button.Text = "Push me";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Running_Button
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 492);
+            this.Controls.Add(this.button);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Running_Button";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button;
+    }
+}
+
---
+++ lab7/lab7.1/Button.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab7._1
+{
+    public partial class Running_Button : Form
+    {
+        public Running_Button()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Поздравляем! Вы смогли нажать на кнопку!");
+        }
+
+        
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.X > button.Left - 10 && e.X < button.Left + button.Width + 10)
+            {
+                if (e.X >= (button.Left + (button.Width / 2)))
+                    button.Left -= 10;
+                else
+                    button.Left += 10;
+            }
+
+            if (e.Y >= button.Top - 10 && e.Y <= button.Top + button.Height + 10)
+            {
+                if (e.Y >= (button.Top + (button.Height / 2)))
+                    button.Top -= 10;
+                else
+                    button.Top += 10;
+            }
+            if (button.Left < 0)
+                button.Left = 0;
+            if ((button.Left + button.Width) > this.ClientSize.Width)
+                button.Left = this.ClientSize.Width - button.Width;
+            if (button.Top < 0)
+                button.Top = 0;
+            if ((button.Top + button.Height) > this.ClientSize.Height)
+                button.Top = this.ClientSize.Height - button.Height;
+        }
+
+    }
+}
